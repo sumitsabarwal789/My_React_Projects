9f4e2e0c57919e799e3f7f99ad26281816dffc5a
sumitsabarwal789 <91772678+sumitsabarwal789@users.noreply.github.com>
2024-01-14 00:53:08 +0530

    changed provider to main.jsx

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -1,4 +1,3 @@
-import UserContextProvider from "./context/UserContextProvider";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 import InsideCards from "./components/InsideCards";
@@ -6,14 +5,12 @@ import Forcast from "./components/Forcast";
 
 export default function App() {
   return (
-    <UserContextProvider>
-      <div className="w-full h-full bg-[#1F2041] text-center text-white mt-12">
-        <Navbar />
-        <Card />
-        <Forcast />
-        <InsideCards />
-        {/* <Errorr /> */}
-      </div>
-    </UserContextProvider>
+    <div className="w-full h-full bg-[#1F2041] text-center text-white mt-12">
+      <Navbar />
+      <Card />
+      <Forcast />
+      <InsideCards />
+      {/* <Errorr /> */}
+    </div>
   );
 }
